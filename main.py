c932865021ce8ba7f088a39afb714ae3138070ff
dci-student <asmaa.shehata@dci.student.org>
2023-10-19 11:13:44 +0200

    Try me

---
+++ main.py
@@ -0,0 +1,23 @@
+# this is function to write comments 
+print('Save Palestine')
+
+'''
+this is the first line of comment
+this is the second one
+this is the third one
+'''
+
+'''
+print("what is your name: ")
+
+name = input()
+print(name)
+'''
+
+The_capital_of_palestine = "AL Qudsssss"
+print(The_capital_of_palestine.isupper())
+
+mybelieve = "Free Palestine"
+def mybelieve():
+    print("free")
+    input()
